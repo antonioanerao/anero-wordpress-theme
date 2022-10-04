abca64cdfe8f8b2dff24d70dd97dc9d29a308fb6
Antonio Anerão <anerao.junior@gmail.com>
2022-10-03 21:17:47 -0500

    Adicionado footer settings

--- inc/customizer.php
+++ inc/customizer.php
@@ -3,6 +3,12 @@
 function anero_customizer($wp_customize) {
 	//Theme header Section
 	require_once('customizer/header/theme-header-section.php');
+	
+	//Panel Footer Settings
+	require_once('customizer/footer/theme-panel-footer-settings.php');
+	
+	/* Social Media section start */
+	require_once('customizer/footer/theme-social-media-settings.php');
 }
 add_action( 'customize_register', 'anero_customizer' );
 
